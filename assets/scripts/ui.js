82638f11f233f0d3e85c6257a3b22fbf11c5cf7a
greg goldman <gregory.goldman97@gmail.com>
2021-06-03 14:08:54 -0400

    Add failure messages
    Adds simple failure messages for the auth user actions

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -3,18 +3,36 @@ const store = require('./store.js')
 const signUpSuccess = () => {
   $('#sign-up-section').hide()
 }
+const signUpFailure = () => {
+  $('#message').text('Failure')
+}
 const signInSuccess = (res) => {
   store.token = res.user.token
   console.log('in signInSucces, ', store.token)
   $('#sign-in-section').hide()
 }
+const signInFailure = () => {
+  $('#message').text('Failure')
+}
 const changePasswordSuccess = () => {
   $('#change-password-section').hide()
 }
-$('#signOut').hide()
-
+const changePasswordFailure = () => {
+  $('#message').text('Failure')
+}
+const signOutSuccess = () => {
+  $('#sign-in-section').show()
+}
+const signOutFailure = () => {
+  $('#message').text('Failure')
+}
 module.exports = {
   signUpSuccess,
+  signUpFailure,
   signInSuccess,
-  changePasswordSuccess
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
 }
